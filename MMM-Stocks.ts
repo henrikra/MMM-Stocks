5b349e5ded51f0517efb6d747d0424043c97a01d
Henrik Raitasola <henrik.raitasola@gmail.com>
2020-03-01 22:48:24 +0200

    Add basic fetching for IEX api

--- MMM-Stocks.ts
+++ MMM-Stocks.ts
@@ -1,11 +1,37 @@
-Module.register<{ text: string }>("MMM-Stocks", {
-	defaults: {
-		text: "Sofia!"
-	},
+type Quote = { changePercent: number };
 
-	getDom: function() {
-		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
-		return wrapper;
+const createIEXApi = function(config: Config) {
+	return {
+		quote(): Promise<Quote> {
+			return fetch(
+				"https://cloud.iexapis.com/stable/stock/CCL/quote?token=" +
+					config.apiKey
+			).then(res => res.json());
+		}
+	};
+};
+
+type Config = { apiKey: string };
+
+Module.register<Config, { IEXApi: ReturnType<typeof createIEXApi> }>(
+	"MMM-Stocks",
+	{
+		getDom: function() {
+			var wrapper = document.createElement("div");
+			wrapper.innerHTML = "Hellurei";
+			return wrapper;
+		},
+
+		start: function() {
+			this.IEXApi = createIEXApi(this.config);
+
+			this.IEXApi.quote()
+				.then(response => {
+					console.log("vaihto", response.changePercent);
+				})
+				.catch(error => {
+					console.log("erroriiii", error);
+				});
+		}
 	}
-});
+);
--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,14 @@
-type ModuleConfig<T> = {
+type ModuleConfig<T, K> = {
 	defaults?: T;
-	getDom(this: ModuleThis<T>): HTMLDivElement;
+	getDom(this: ModuleThis<T> & K): HTMLDivElement;
+	start(this: ModuleThis<T> & K): void;
 	[x: string]: any;
 };
 
 type ModuleThis<T> = { config: T };
 
 type Module = {
-	register<T>(moduleName: string, module: ModuleConfig<T>): void;
+	register<T, K>(moduleName: string, module: ModuleConfig<T, K>): void;
 };
 
 declare var Module: Module;
