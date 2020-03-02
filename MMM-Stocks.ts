4883f53214bca6921067bda6ea9fe05775a95612
Henrik Raitasola <henrik.raitasola@gmail.com>
2020-03-02 19:32:02 +0200

    Render stocks to the screen

--- MMM-Stocks.ts
+++ MMM-Stocks.ts
@@ -1,4 +1,9 @@
-type Quote = { changePercent: number };
+type Quote = {
+	companyName: string;
+	symbol: string;
+	changePercent: number;
+	latestPrice: number;
+};
 
 const createIEXApi = function(apiKey: string) {
 	return {
@@ -17,25 +22,112 @@ const createIEXApi = function(apiKey: string) {
 
 type Config = { apiKey: string; stocks: string[] };
 
-Module.register<Config, { IEXApi: ReturnType<typeof createIEXApi> }>(
-	"MMM-Stocks",
+type RequestLoading = { type: "loading" };
+type RequestSuccess<T> = { type: "success"; data: T };
+type RequestError = { type: "error"; error: Error };
+
+type Service<T> = RequestLoading | RequestSuccess<T> | RequestError;
+
+Module.register<
+	Config,
 	{
-		getDom: function() {
-			var wrapper = document.createElement("div");
-			wrapper.innerHTML = "Hellurei";
-			return wrapper;
-		},
-
-		start: function() {
-			this.IEXApi = createIEXApi(this.config.apiKey);
-
-			this.IEXApi.quoteBatch(this.config.stocks)
-				.then(response => {
-					console.log("result", response);
-				})
-				.catch(error => {
-					console.log("erroriiii", error);
-				});
-		}
+		IEXApi: ReturnType<typeof createIEXApi>;
+		state: { stocks: Service<Quote[]> };
 	}
-);
+>("MMM-Stocks", {
+	getStyles: function() {
+		return [this.file("styles.css")];
+	},
+	getDom: function() {
+		var wrapper = document.createElement("div");
+
+		if (this.state.stocks.type === "success") {
+			const list = document.createElement("ul");
+
+			this.state.stocks.data.forEach(stock => {
+				const listItem = document.createElement("li");
+
+				const companyInfo = document.createElement("div");
+
+				const stockSymbol = document.createElement("div");
+				const symbolText = document.createTextNode(stock.symbol);
+				stockSymbol.appendChild(symbolText);
+				stockSymbol.classList.add("small", "bright");
+
+				const companyName = document.createElement("div");
+				const companyNameText = document.createTextNode(stock.companyName);
+				companyName.appendChild(companyNameText);
+				companyName.classList.add("small");
+
+				companyInfo.appendChild(stockSymbol);
+				companyInfo.appendChild(companyName);
+
+				listItem.appendChild(companyInfo);
+
+				const numbers = document.createElement("div");
+
+				const currentPrice = document.createElement("div");
+				const currentPriceText = document.createTextNode(
+					stock.latestPrice.toString()
+				);
+				currentPrice.appendChild(currentPriceText);
+				currentPrice.classList.add("small", "align-right");
+
+				const priceChange = document.createElement("div");
+				const priceChangeText = document.createTextNode(
+					stock.changePercent.toLocaleString("en", {
+						style: "percent",
+						minimumFractionDigits: 2
+					})
+				);
+				priceChange.appendChild(priceChangeText);
+				priceChange.classList.add("small", "align-right");
+
+				if (stock.changePercent < 0) {
+					priceChange.classList.add("red");
+				}
+
+				if (stock.changePercent > 0) {
+					priceChange.classList.add("green");
+				}
+
+				numbers.appendChild(currentPrice);
+				numbers.appendChild(priceChange);
+
+				listItem.appendChild(numbers);
+
+				list.appendChild(listItem);
+			});
+
+			wrapper.appendChild(list);
+		} else if (this.state.stocks.type === "loading") {
+			wrapper.innerHTML = "Loading";
+		} else {
+			wrapper.innerHTML = "Error";
+		}
+
+		return wrapper;
+	},
+
+	start: function() {
+		this.state = { stocks: { type: "loading" } };
+
+		this.IEXApi = createIEXApi(this.config.apiKey);
+
+		this.IEXApi.quoteBatch(this.config.stocks)
+			.then(response => {
+				this.state = {
+					stocks: {
+						type: "success",
+						data: Object.values(response).map(lol => lol.quote)
+					}
+				};
+				this.updateDom();
+			})
+			.catch(error => {
+				this.state = { stocks: { type: "error", error } };
+				this.updateDom();
+				console.log("error", error);
+			});
+	}
+});
--- global.d.ts
+++ global.d.ts
@@ -2,10 +2,15 @@ type ModuleConfig<T, K> = {
 	defaults?: T;
 	getDom(this: ModuleThis<T> & K): HTMLDivElement;
 	start(this: ModuleThis<T> & K): void;
+	getStyles?(this: ModuleThis<T> & K): string[];
 	[x: string]: any;
 };
 
-type ModuleThis<T> = { config: T };
+type ModuleThis<T> = {
+	config: T;
+	updateDom(speed?: number): void;
+	file(filename: string): string;
+};
 
 type Module = {
 	register<T, K>(moduleName: string, module: ModuleConfig<T, K>): void;
